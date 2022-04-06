c2fd097f5c86b6d2bd98054293ba0a23d31fdebd
sr_boro <11045297+oscarda@users.noreply.github.com>
2022-04-06 22:59:15 +0200

    Add files via upload

---
+++ Tiempo/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace sf;
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+
+	Clock cuenta_atras;
+	Time tiempo_pasado;
+	int segundos = 0;
+	while(true) {
+
+		tiempo_pasado = cuenta_atras.getElapsedTime();
+		segundos = tiempo_pasado.asSeconds();
+		cout << segundos/60 << ":" <<segundos%60 <<endl; 
+	}
+	return 0;
+}
+
